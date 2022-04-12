964a1e08ee0649d1fd5bab47b85f86a54ba3efd9
zy-xmz <zy_20131114@163.com>
2022-04-13 06:28:36 +0800

    初步创建目录结构

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
     </div>
   );
+}
+
+function Layout() {
+  return (
+    <div>
+      <div>
+      <div><Link to="/">home</Link></div>
+      <div><Link to="/login">去登录</Link></div>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+  <h1>登录</h1>
+</div>
+  )
 }
 
 export default App;
