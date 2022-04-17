e234032143114ce5f7985b74f7a9fabf59262fd1
zy-xmz <zy_20131114@163.com>
2022-04-18 06:51:01 +0800

    搭建静态页面

--- src/pages/happy/index.js
+++ src/pages/happy/index.js
@@ -1,10 +1,24 @@
 import '../../static/css/happy.scss'
-import { LeftSquareOutlined, RightSquareOutlined, MoreOutlined } from '@ant-design/icons'
+import {
+  LeftSquareOutlined,
+  RightSquareOutlined,
+  MoreOutlined,
+  PauseOutlined,
+  HeartOutlined,
+  CaretRightOutlined
+} from '@ant-design/icons'
 import { Image } from 'antd'
 import movie01 from '../../static/img/movie01.png'
 import movie02 from '../../static/img/movie02.webp'
 import movie03 from '../../static/img/movie03.webp'
 import movie04 from '../../static/img/movie04.webp'
+import head from '../../static/img/music_head.webp'
+import head2 from '../../static/img/music_head2.webp'
+import head3 from '../../static/img/music_head3.png'
+import mv1 from '../../static/img/mv1.webp'
+import mv2 from '../../static/img/mv2.jpg'
+import mv3 from '../../static/img/mv3.webp'
+import mv4 from '../../static/img/mv4.webp'
 
 export default function Happy () {
   return (
@@ -13,10 +27,7 @@ export default function Happy () {
       <div className="movie-box">
         {/* 标题 */}
         <div className="title">
-          <div class="txt">
-            <h3>你可能喜欢的电影</h3>
-            <p>根据你的观影历史推荐</p>
-          </div>
+          <h3>喜欢的电影</h3>
           <div className="arrows">
             <LeftSquareOutlined style={{ color: '#ccc', fontSize: 26 }} />
             <RightSquareOutlined style={{ color: '#ccc', fontSize: 26 }} />
@@ -66,22 +77,73 @@ export default function Happy () {
       </div>
       {/* 下半部分 */}
       <div className="below-box">
-        <div></div>
+        <div className="video">
+          <h3>喜欢的视频</h3>
+          <div className="list">
+            <ul>
+              <li>
+                <div className="icon">
+                  <CaretRightOutlined style={{ color: 'white', fontSize: 30 }}/>
+                </div>
+                <img src={ mv1 } alt="" />
+              </li>
+              <li>
+                <div className="icon">
+                  <CaretRightOutlined style={{ color: 'white', fontSize: 30 }}/>
+                </div>
+                <img src={ mv2 } alt="" />
+              </li>
+              <li>
+                <div className="icon">
+                  <CaretRightOutlined style={{ color: 'white', fontSize: 30 }}/>
+                </div>
+                <img src={ mv3 } alt="" />
+              </li>
+              <li>
+                <div className="icon">
+                  <CaretRightOutlined style={{ color: 'white', fontSize: 30 }}/>
+                </div>
+                <img src={ mv4 } alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
         {/* 音乐 */}
         <div className="music">
-          <div className="title">
-            <div class="txt">
-              <h3>音乐库</h3>
-              <p>根据你的喜好推荐</p>
-            </div>
-            <div className="operate">
-              <MoreOutlined style={{ color: '#ccc', fontSize: 22 }} />
-            </div>
-          </div>
+          <h3>喜欢的音乐</h3>
           {/* 音乐列表 */}
           <div className="list">
             <ul>
-              <li></li>
+              <li>
+                <PauseOutlined />
+                <img src={ head } alt="" />
+                <div className="txt">
+                  <p>17岁</p>
+                  <p>刘德华</p>
+                </div>
+                <span className="duration">4:02</span>
+                <HeartOutlined style={{ fontSize: 18 }}/>
+              </li>
+              <li>
+                <PauseOutlined />
+                <img src={ head2 } alt="" />
+                <div className="txt">
+                  <p>一生所爱</p>
+                  <p>卢冠廷</p>
+                </div>
+                <span className="duration">4:13</span>
+                <HeartOutlined style={{ fontSize: 18 }}/>
+              </li>
+              <li>
+                <PauseOutlined />
+                <img src={ head3 } alt="" />
+                <div className="txt">
+                  <p>盛夏的果实</p>
+                  <p>莫文蔚</p>
+                </div>
+                <span className="duration">4:10</span>
+                <HeartOutlined style={{ fontSize: 18 }}/>
+              </li>
             </ul>
           </div>
         </div>
