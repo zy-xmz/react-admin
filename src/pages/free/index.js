925ebf079129dbddb4e9bd3805aee5f7d7eef2f4
zy-xmz <zy_20131114@163.com>
2022-04-21 06:27:39 +0800

    初步开发‘自由’页

--- src/pages/free/index.js
+++ src/pages/free/index.js
@@ -1,8 +1,51 @@
+import '../../static/css/free.scss'
+import { PlusCircleOutlined } from '@ant-design/icons'
+import IconFont from '../../components/IconFont'
+
 // 目标馆
 export default function Free () {
   return (
-    <div>
-      自由
+    <div className="free">
+      <div className="up">
+        {/* 目标列表 */}
+        <div className="target">
+          <div className="title">
+            <h3>有目标</h3>
+            <PlusCircleOutlined style={{ fontSize: 16 }}/>
+          </div>
+          <ul>
+            <li>
+              <p>2022-03-15 ~ 2022-04-15</p>
+              <h6>学习flutter</h6>
+              <div className="icons">
+                <IconFont type="icon-success" style={{ fontSize: 18 }}/>
+                <IconFont type="icon-shibai" style={{ fontSize: 20 }}/>
+                <IconFont type="icon-delete" style={{ fontSize: 18 }}/>
+              </div>
+            </li>
+            <li>
+              <p>2022-03-15 ~ 2022-04-15</p>
+              <h6>学习flutter</h6>
+              <div className="icons">
+                <IconFont type="icon-success" style={{ fontSize: 18 }}/>
+                <IconFont type="icon-shibai" style={{ fontSize: 20 }}/>
+                <IconFont type="icon-delete" style={{ fontSize: 18 }}/>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* 学习课程 */}
+        <div className="study">
+
+        </div>
+        {/* 健身课程 */}
+        <div className="keep-fit">
+
+        </div>
+      </div>
+      <div className="down">
+
+      </div>
     </div>
   )
 }
